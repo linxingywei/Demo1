bd7e8122e7a959a5d3499bf235b0a76c4f72da11
yanglin <linxingywei@sina.com>
2017-10-26 00:18:36 +0800

    第一次提交

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+        List a = new ArrayList();
+        for (Object o : a) {
+            System.out.println(o);
+        }
+    }
+
+}
