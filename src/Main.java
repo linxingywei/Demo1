0959bc6965e53556aa48ef09c02c21a462e339cf
yanglin <linxingywei@sina.com>
2017-10-26 00:28:30 +0800

    第2次提交

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         System.out.println();
-        List a = new ArrayList();
+
         for (Object o : a) {
             System.out.println(o);
         }
